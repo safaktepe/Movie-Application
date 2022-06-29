ee0d221477a564dd6eacf1380b1304a8f730f5d6
safaktepe <safaktepemert@gmail.com>
2022-06-29 20:28:13 +0300

    Bug fix on splash

--- Movie Application/View/SplashVC.swift
+++ Movie Application/View/SplashVC.swift
@@ -11,20 +11,28 @@ class SplashVC: UIViewController {
     
     private lazy var createTaskButton: UIButton  = {
             let button = UIButton()
-            button.setTitle("Create", for: .normal)
+            button.setTitle("Devam", for: .normal)
             button.backgroundColor = .red
             button.addTarget(self, action: #selector(createButtonClicked), for: .touchUpInside)
             return button
         }()
-
+    var geciciLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Buraya splash ekranı gelecek"
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+            view.addSubview(geciciLabel)
             view.backgroundColor = .white
             view.addSubview(createTaskButton)
+            geciciLabel.snp.makeConstraints { make in
+                make.centerX.equalTo(view)
+                make.centerY.equalTo(view)
+            }
             createTaskButton.snp.makeConstraints { make in
-            make.top.equalTo(view).offset(50)
-            make.left.equalTo(view).offset(50)
+            make.top.equalTo(geciciLabel.snp.bottom).offset(5)
+            make.centerX.equalTo(view)
             make.width.height.equalTo(100)
            
         }
